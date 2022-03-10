9d6f009445fe8429ee7914b3da785b3be14f905e
Yuri-YuzuChaN <806235364@qq.com>
2022-03-10 18:36:42 +0800

    更新段位显示

--- libraries/maimai_best_40.py
+++ libraries/maimai_best_40.py
@@ -206,57 +206,57 @@ class DrawBest(object):
     
     def _findMatchLevel(self) -> str:
         addrating = self.rankRating
-        t = "01"
-        if addrating >= 0 and addrating < 250:
-            t = "01"
-        elif addrating >= 250 and addrating < 500:
-            t = "02"
-        elif addrating >= 500 and addrating < 750:
-            t = "03"
-        elif addrating >= 750 and addrating < 1000:
-            t = "04"
-        elif addrating >= 1000 and addrating < 1200:
-            t = "05"
-        elif addrating >= 1200 and addrating < 1400:
-            t = "06"
-        elif addrating >= 1400 and addrating < 1500:
-            t = "07"
-        elif addrating >= 1500 and addrating < 1600:
-            t = "08"
-        elif addrating >= 1600 and addrating < 1700:
-            t = "09"
-        elif addrating >= 1700 and addrating < 1800:
-            t = "10"
-        elif addrating >= 1800 and addrating < 1850:
-            t = "11"
-        elif addrating >= 1850 and addrating < 1900:
-            t = "12"
-        elif addrating >= 1900 and addrating < 1950:
-            t = "13"
-        elif addrating >= 1950 and addrating < 2000:
-            t = "14"
-        elif addrating >= 2000 and addrating < 2010:
-            t = "15"
-        elif addrating >= 2010 and addrating < 2020:
-            t = "16"
-        elif addrating >= 2020 and addrating < 2030:
-            t = "17"
-        elif addrating >= 2030 and addrating < 2040:
-            t = "18"
-        elif addrating >= 2040 and addrating < 2050:
-            t = "19"
-        elif addrating >= 2050 and addrating < 2060:
-            t = "20"
-        elif addrating >= 2060 and addrating < 2070:
-            t = "21"
-        elif addrating >= 2070 and addrating < 2080:
-            t = "22"
-        elif addrating >= 2080 and addrating < 2090:
-            t = "23"
-        elif addrating >= 2090 and addrating < 2100:
-            t = "24"
-        elif addrating >= 2100:
-            t = "25"
+        t = '01'
+        if addrating < 250:
+            t = '01'
+        elif addrating < 500:
+            t = '02'
+        elif addrating < 750:
+            t = '03'
+        elif addrating < 1000:
+            t = '04'
+        elif addrating < 1200:
+            t = '05'
+        elif addrating < 1400:
+            t = '06'
+        elif addrating < 1500:
+            t = '07'
+        elif addrating < 1600:
+            t = '08'
+        elif addrating < 1700:
+            t = '09'
+        elif addrating < 1800:
+            t = '10'
+        elif addrating < 1850:
+            t = '11'
+        elif addrating < 1900:
+            t = '12'
+        elif addrating < 1950:
+            t = '13'
+        elif addrating < 2000:
+            t = '14'
+        elif addrating < 2010:
+            t = '15'
+        elif addrating < 2020:
+            t = '16'
+        elif addrating < 2030:
+            t = '17'
+        elif addrating < 2040:
+            t = '18'
+        elif addrating < 2050:
+            t = '19'
+        elif addrating < 2060:
+            t = '20'
+        elif addrating < 2070:
+            t = '21'
+        elif addrating < 2080:
+            t = '22'
+        elif addrating < 2090:
+            t = '23'
+        elif addrating < 2100:
+            t = '24'
+        else:
+            t = '25'
 
         return f'UI_CMN_MatchLevel_{t}.png'
 
