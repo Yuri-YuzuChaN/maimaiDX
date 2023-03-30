dea2439327eb0c9bd356c36ca11cc2b59cd420ef
Yuri-YuzuChaN <806235364@qq.com>
2023-03-30 11:54:18 +0800

    fix

--- libraries/maimaidx_project.py
+++ libraries/maimaidx_project.py
@@ -196,7 +196,7 @@ async def query_chart_data(match: Match) -> str:
 TAP: {chart.tap}
 HOLD: {chart.hold}
 SLIDE: {chart.slide}
-BREAK: {chart.touch}
+BREAK: {chart.brk}
 谱师: {chart.charter}
 难易度参考: {stats['tag'] if 'tag' in stats else '无'}'''
             else:
