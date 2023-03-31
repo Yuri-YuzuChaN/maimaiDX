8126ab7f71093cb34aeb9a1f686868a2c8f504fd
zhanbao2000 <zhanbao2000@gmail.com>
2023-03-31 19:13:09 +0800

    添加mokabot插件管理器

--- __init__.py
+++ __init__.py
@@ -7,3 +7,11 @@ static = os.path.join(os.path.dirname(__file__), 'static')
 __all__ = ['maimai']
 
 from . import *
+from nonebot.plugin import PluginMetadata
+
+__plugin_meta__ = PluginMetadata(
+    name='maimaiDX',
+    description='移植自 xybot 及 mai-bot 开源项目，基于 HoshinoBotV2 和 nonebot2 的街机音游 舞萌DX 的查询插件',
+    usage='',
+    extra={'enable_on_default': True}
+)
