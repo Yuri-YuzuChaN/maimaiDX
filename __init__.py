7c065fcaf6802a680a19158957aff1ee0be64adc
zhanbao2000 <zhanbao2000@gmail.com>
2023-03-31 19:14:19 +0800

    默认关闭maimaiDX插件

--- __init__.py
+++ __init__.py
@@ -13,5 +13,5 @@ __plugin_meta__ = PluginMetadata(
     name='maimaiDX',
     description='移植自 xybot 及 mai-bot 开源项目，基于 HoshinoBotV2 和 nonebot2 的街机音游 舞萌DX 的查询插件',
     usage='',
-    extra={'enable_on_default': True}
+    extra={'enable_on_default': False}
 )
