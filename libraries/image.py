f787b102dacb3e02aba6e6c3cab031f70d84747c
Yuri-YuzuChaN <806235364@qq.com>
2023-07-22 17:41:00 +0800

    适配 `pillow 10.0.0` 版本模块

--- libraries/image.py
+++ libraries/image.py
@@ -30,15 +30,15 @@ def text_to_image(text: str) -> Image.Image:
     text_list = text.split('\n')
     max_width = 0
     for text in text_list:
-        w, h = font.getsize(text)
-        max_width = max(max_width, w)
+        l, t, r, b = font.getbbox(text)
+        max_width = max(max_width, r)
     wa = max_width + padding * 2
-    ha = h * len(text_list) + margin * (len(text_list) - 1) + padding * 2
+    ha = b * len(text_list) + margin * (len(text_list) - 1) + padding * 2
     i = Image.new('RGB', (wa, ha), color=(255, 255, 255))
     draw = ImageDraw.Draw(i)
     for j in range(len(text_list)):
         text = text_list[j]
-        draw.text((padding, padding + j * (margin + h)), text, font=font, fill=(0, 0, 0))
+        draw.text((padding, padding + j * (margin + b)), text, font=font, fill=(0, 0, 0))
     return i
 
 
